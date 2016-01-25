c627e824f20d70fd98099986cb1bd87f2c0bb83f
Kiseop Lee <kiseop.lee@gmail.com>
2016-01-25 23:59:25 +0900

    Chapter9 예제 코드 및 주석 example9까지 작성, example10은 주석은 미작성

--- document/AngularJS/source/lks/Chapter07/components/cart/cart.js
+++ document/AngularJS/source/lks/Chapter07/components/cart/cart.js
@@ -35,6 +35,26 @@ angular.module("cart", [])
 .directive("cartSummary", function (cart) {
 	return {
 		restrict: "E",
-		templateUrl: "components/cart/cartSummary.html"
-	}
+		templateUrl: "components/cart/cartSummary.html",
+		controller: function ($scope) {
+
+			var cartData = cart.getProducts();
+
+			$scope.total = function () {
+				var total = 0;
+				for (var i = 0; i < cartData.length; i++) {
+					total += (cartData[i].price * cartData[i].count);
+				}
+				return total;
+			}
+
+			$scope.itemCount = function () {
+				var total = 0;
+				for (var i = 0; i < cartData.length; i++) {
+					total += cartData[i].count;
+				}
+				return total;
+			}
+		}
+	};
 });
